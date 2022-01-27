6496d6f08483f69ca9c301cdb60ba3f05d7b4d17
Manish Kumar <manish86.mishra@gmail.com>
2022-01-27 00:33:01 -0600

    stop testing shimmer view

--- Sources/MovieDB/MovieList/Views/PosterCell.swift
+++ Sources/MovieDB/MovieList/Views/PosterCell.swift
@@ -37,15 +37,15 @@ final class PosterCell: UITableViewCell {
         posterView.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
         posterView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
         posterView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-        self.addShadow(shadowColor: .darkGray, offSet: self.intrinsicContentSize, opacity: 0.8, shadowRadius: 8.0, cornerRadius: 8.0, corners: .allCorners)
-        self.configureAndStartShimmering()
+//        self.addShadow(shadowColor: .darkGray, offSet: self.intrinsicContentSize, opacity: 0.8, shadowRadius: 8.0, cornerRadius: 8.0, corners: .allCorners)
+//        self.configureAndStartShimmering()
     }
     
     func configure(cellViewModel: PosterCellViewModel) {
         guard (posterView.image != nil) else {
             cellViewModel.fetchImage(callback: {(status, data) in
                 DispatchQueue.main.async {
-                    self.stopShimmering()
+                   // self.stopShimmering()
                     switch status {
                     case .success:
                         guard let data = data else { return}
