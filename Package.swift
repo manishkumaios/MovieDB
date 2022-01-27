d55158eace92ae5916703191dcdf6a9defa23a0a
Manish Kumar <manish86.mishra@gmail.com>
2022-01-27 15:39:04 -0600

    Updated package configurations

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MovieDB",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MovieDB",
-            dependencies: [.byNameItem(name: "CorePackage", condition: nil)],
+            dependencies: ["CorePackage"],
             path: "Sources",
             swiftSettings: [
                     .define("SPM")
